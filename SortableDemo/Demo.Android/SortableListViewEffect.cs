715013d7ee003124842934f2fe65a4beaf7b32b7
DESKTOP-K4FBQRK\piete <pieterjandeclippel@msn.com>
2020-10-19 00:05:15 +0200

    Working version with logic from library

---
+++ SortableDemo/Demo.Android/SortableListViewEffect.cs
@@ -0,0 +1,50 @@
+﻿//using MintPlayer.Xamarin.Forms.SortableListView.Platforms.Android;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using Xamarin.Forms;
+//using Xamarin.Forms.Platform.Android;
+
+//[assembly: ResolutionGroupName("MintPlayer")]
+//[assembly: ExportEffect(typeof(MintPlayer.Xamarin.Forms.SortableListView.Demo.Droid.SortableListViewEffect), nameof(MintPlayer.Xamarin.Forms.SortableListView.Demo.Droid.SortableListViewEffect))]
+//namespace MintPlayer.Xamarin.Forms.SortableListView.Demo.Droid
+//{
+//    public class SortableListViewEffect : PlatformEffect
+//    {
+//        private DragListAdapter dragListAdapter = null;
+//        public SortableListViewEffect()
+//        {
+//        }
+
+//        protected override void OnAttached()
+//        {
+//            var element = Element as ListView;
+
+//            if (Control is global::Android.Widget.ListView listView)
+//            {
+//                dragListAdapter = new DragListAdapter(listView, element);
+//                listView.Adapter = dragListAdapter;
+//                listView.SetOnDragListener(dragListAdapter);
+//                listView.OnItemLongClickListener = dragListAdapter;
+//            }
+//        }
+
+//        protected override void OnDetached()
+//        {
+//            if (Control is global::Android.Widget.ListView listView)
+//            {
+//                listView.Adapter = dragListAdapter.WrappedAdapter;
+
+//                // TODO: Remove the attached listeners
+//            }
+//        }
+
+//        protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs args)
+//        {
+//            if (args.PropertyName == Sorting.IsSortableProperty.PropertyName)
+//            {
+//                dragListAdapter.DragDropEnabled = Sorting.GetIsSortable(Element);
+//            }
+//        }
+//    }
+//}
---
+++ SortableDemo/Demo/Sorting.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Xamarin.Forms;
+
+//namespace MintPlayer.Xamarin.Forms.SortableListView.Demo
+//{
+//    public static class Sorting
+//    {
+//        public static readonly BindableProperty IsSortableProperty =
+//            BindableProperty.CreateAttached("IsSortable", typeof(bool), typeof(SortableListViewEffect), false,
+//                propertyChanged: OnIsSortableChanged);
+
+//        public static bool GetIsSortable(BindableObject view)
+//        {
+//            return (bool)view.GetValue(IsSortableProperty);
+//        }
+
+//        public static void SetIsSortable(BindableObject view, bool value)
+//        {
+//            view.SetValue(IsSortableProperty, value);
+//        }
+
+//        static void OnIsSortableChanged(BindableObject bindable, object oldValue, object newValue)
+//        {
+//            var view = bindable as ListView;
+//            if (view == null)
+//            {
+//                return;
+//            }
+
+//            if (!view.Effects.Any(item => item is SortableListViewEffect))
+//            {
+//                view.Effects.Add(new SortableListViewEffect());
+//            }
+//        }
+//    }
+//}
